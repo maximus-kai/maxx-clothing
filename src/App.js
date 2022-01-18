619c90ae699d23d28c0b97860494925cf52e9a3e
Dairo Tomiwa <38611128+maximus-kai@users.noreply.github.com>
2022-01-18 15:51:04 +0100

    first add for this repo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Amazing Stuff!!! <br/>
+         Straight out of a movie 
         </p>
         <a
           className="App-link"
