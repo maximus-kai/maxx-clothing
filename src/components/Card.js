8bf4a15554f5a3cabdc17f5a4d8a4f6640d541ab
Dairo Tomiwa <38611128+maximus-kai@users.noreply.github.com>
2022-01-19 14:15:46 +0100

    latest changes from vs code

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,12 +2,36 @@ import React from 'react';
 
 const Card =()=> {
     return(
-        <div class="card" style="width: 18rem;">
-             {/* <img src="..." class="card-img-top" alt="..."/>
-             <div class="card-body">
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-             </div> */}
+        <div class="card-group">
+            <div className="card" style={{width: '18rem'}} >
+                <img src="https://www.cnet.com/a/img/CSTqzAl5wJ57HHyASLD-a0vS2O0=/940x528/2021/04/05/9e065d90-51f2-46c5-bd3a-416fd4983c1a/elantra-1080p.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}} >
+                <img src="https://www.cnet.com/a/img/CSTqzAl5wJ57HHyASLD-a0vS2O0=/940x528/2021/04/05/9e065d90-51f2-46c5-bd3a-416fd4983c1a/elantra-1080p.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}} >
+                <img src="https://www.cnet.com/a/img/CSTqzAl5wJ57HHyASLD-a0vS2O0=/940x528/2021/04/05/9e065d90-51f2-46c5-bd3a-416fd4983c1a/elantra-1080p.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
+            <div className="card" style={{width: '18rem'}} >
+                <img src="https://www.cnet.com/a/img/CSTqzAl5wJ57HHyASLD-a0vS2O0=/940x528/2021/04/05/9e065d90-51f2-46c5-bd3a-416fd4983c1a/elantra-1080p.jpg" className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>
     );
 }
 export default Card;
+// <div class="card" style="width: 18rem;">
+     {/* 
+     */}
+{/* </div> */}
